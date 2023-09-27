9d536f3efb8d3ca7fbb4f7c9f996d76f21cb1b1a
Martin Egli <m_wuerms@gmx.net>
2023-09-27 15:42:24 +0200

    LT1: new stm32cubeide project created

---
+++ stmcubeide/LT1/Core/Inc/main.h
@@ -0,0 +1,124 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32l4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define ALM0_Pin GPIO_PIN_0
+#define ALM0_GPIO_Port GPIOH
+#define ALM1_Pin GPIO_PIN_1
+#define ALM1_GPIO_Port GPIOH
+#define DISP_ROW0_Pin GPIO_PIN_0
+#define DISP_ROW0_GPIO_Port GPIOA
+#define DISP_ROW1_Pin GPIO_PIN_1
+#define DISP_ROW1_GPIO_Port GPIOA
+#define DISP_ROW2_Pin GPIO_PIN_2
+#define DISP_ROW2_GPIO_Port GPIOA
+#define DISP_ROW3_Pin GPIO_PIN_3
+#define DISP_ROW3_GPIO_Port GPIOA
+#define DISP_ROW4_Pin GPIO_PIN_4
+#define DISP_ROW4_GPIO_Port GPIOA
+#define DISP_ROW5_Pin GPIO_PIN_5
+#define DISP_ROW5_GPIO_Port GPIOA
+#define DISP_ROW6_Pin GPIO_PIN_6
+#define DISP_ROW6_GPIO_Port GPIOA
+#define DISP_COL_SER_Pin GPIO_PIN_7
+#define DISP_COL_SER_GPIO_Port GPIOA
+#define BTN_UP1_Pin GPIO_PIN_0
+#define BTN_UP1_GPIO_Port GPIOB
+#define BTN_UP2_Pin GPIO_PIN_1
+#define BTN_UP2_GPIO_Port GPIOB
+#define BTN_UP3_Pin GPIO_PIN_2
+#define BTN_UP3_GPIO_Port GPIOB
+#define SW1_Pin GPIO_PIN_10
+#define SW1_GPIO_Port GPIOB
+#define SW2_Pin GPIO_PIN_11
+#define SW2_GPIO_Port GPIOB
+#define SW3_Pin GPIO_PIN_12
+#define SW3_GPIO_Port GPIOB
+#define SW4_Pin GPIO_PIN_13
+#define SW4_GPIO_Port GPIOB
+#define DISP_COL_SRCLK_Pin GPIO_PIN_8
+#define DISP_COL_SRCLK_GPIO_Port GPIOA
+#define DISP_COL_RCLK_Pin GPIO_PIN_9
+#define DISP_COL_RCLK_GPIO_Port GPIOA
+#define DISP_COL_SRCLR_Pin GPIO_PIN_10
+#define DISP_COL_SRCLR_GPIO_Port GPIOA
+#define DISP_COL_OE_Pin GPIO_PIN_15
+#define DISP_COL_OE_GPIO_Port GPIOA
+#define BTN_UP4_Pin GPIO_PIN_3
+#define BTN_UP4_GPIO_Port GPIOB
+#define BTN_PLAY_Pin GPIO_PIN_4
+#define BTN_PLAY_GPIO_Port GPIOB
+#define BTN_MODE_Pin GPIO_PIN_5
+#define BTN_MODE_GPIO_Port GPIOB
+#define BTN_STOP_Pin GPIO_PIN_6
+#define BTN_STOP_GPIO_Port GPIOB
+#define CH_STAT_Pin GPIO_PIN_7
+#define CH_STAT_GPIO_Port GPIOB
+#define USB_STAT_Pin GPIO_PIN_8
+#define USB_STAT_GPIO_Port GPIOB
+#define SW0_Pin GPIO_PIN_9
+#define SW0_GPIO_Port GPIOB
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ stmcubeide/LT1/Core/Src/gpio.c
@@ -0,0 +1,104 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOH_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOH, ALM0_Pin|ALM1_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOA, DISP_ROW0_Pin|DISP_ROW1_Pin|DISP_ROW2_Pin|DISP_ROW3_Pin
+                          |DISP_ROW4_Pin|DISP_ROW5_Pin|DISP_ROW6_Pin|DISP_COL_SER_Pin
+                          |DISP_COL_SRCLK_Pin|DISP_COL_RCLK_Pin|DISP_COL_SRCLR_Pin|DISP_COL_OE_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : PHPin PHPin */
+  GPIO_InitStruct.Pin = ALM0_Pin|ALM1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOH, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PAPin PAPin PAPin PAPin
+                           PAPin PAPin PAPin PAPin
+                           PAPin PAPin PAPin PAPin */
+  GPIO_InitStruct.Pin = DISP_ROW0_Pin|DISP_ROW1_Pin|DISP_ROW2_Pin|DISP_ROW3_Pin
+                          |DISP_ROW4_Pin|DISP_ROW5_Pin|DISP_ROW6_Pin|DISP_COL_SER_Pin
+                          |DISP_COL_SRCLK_Pin|DISP_COL_RCLK_Pin|DISP_COL_SRCLR_Pin|DISP_COL_OE_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_HIGH;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin PBPin PBPin */
+  GPIO_InitStruct.Pin = BTN_UP1_Pin|BTN_UP2_Pin|BTN_UP3_Pin|BTN_UP4_Pin
+                          |BTN_PLAY_Pin|BTN_MODE_Pin|BTN_STOP_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLUP;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin PBPin PBPin
+                           PBPin */
+  GPIO_InitStruct.Pin = SW1_Pin|SW2_Pin|SW3_Pin|SW4_Pin
+                          |SW0_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_PULLDOWN;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : PBPin PBPin */
+  GPIO_InitStruct.Pin = CH_STAT_Pin|USB_STAT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
